57819425882bf5ea2da69c563dd3ee483b6cb4ee
Matt Payne <Payne@MattPayne.org>
2018-03-19 19:47:15 -0500

    Added apiKey

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: 'APIKEY',
-        authDomain: 'DEV-APP.firebaseapp.com',
-        databaseURL: 'https://DEV-APP.firebaseio.com',
-        projectId: 'DEV-APP',
-        storageBucket: 'DEV-APP.appspot.com',
-        messagingSenderId: '123456789'
-    }
+        apiKey: "AIzaSyC-GvM19j3hRQuP5hhszHDw4yeCD8ShSr8",
+        authDomain: "acodingbatleaderboard.firebaseapp.com",
+        databaseURL: "https://acodingbatleaderboard.firebaseio.com",
+        projectId: "acodingbatleaderboard",
+        storageBucket: "acodingbatleaderboard.appspot.com",
+        messagingSenderId: "844249876503"
+  }
 };
